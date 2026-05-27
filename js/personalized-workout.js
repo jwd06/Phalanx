24bf07c1593af3ce6da3830e72bdd218f81c7688
Jawad Hossain <jawadhossain3015@gmail.com>
2026-05-27 13:07:38 -0400

    Extended workout exercises db and improve goal handling

--- js/personalized-workout.js
+++ js/personalized-workout.js
@@ -20,7 +20,7 @@ const excercise_db = {
         { name: "Squat",              sets: 3, reps: "8-12" },
         { name: "Romanian Deadlift",  sets: 3, reps: "10-12" },
         { name: "Leg Press",          sets: 3, reps: "10-12" },
-        { name: "Lunges",             sets: 3, reps: "12 each" },
+        { name: "Leg Extension",      sets: 3, reps: "12 each" },
         { name: "Leg Curl",           sets: 3, reps: "10-12" },
         { name: "Calf Raises",        sets: 3, reps: "10-12" }
     ],
@@ -30,23 +30,127 @@ const excercise_db = {
         { name: "Incline Dumbbell Press", sets: 3, reps: "10-12" },
         { name: "Lateral Raises",         sets: 3, reps: "10-12" },
         { name: "Tricep Dips",            sets: 3, reps: "10-12" },
-        { name: "Skull Crushers",         sets: 3, reps: "10-12" }
+        { name: "Tricep Extension / Skull Crushers", sets: 3, reps: "10-12" }
     ],
     "Pull_gain": [
         { name: "Deadlift",               sets: 4, reps: "5-6" },
         { name: "Pull-ups / Lat Pulldown",sets: 4, reps: "8-10" },
         { name: "Barbell Row",            sets: 3, reps: "8-10" },
         { name: "Cable Row",              sets: 3, reps: "10-12" },
-        { name: "Face Pulls",             sets: 3, reps: "12-15" },
-        { name: "Barbell Curl",           sets: 3, reps: "10-12" }
+        { name: "Reverse Flys / Face Pull", sets: 3, reps: "12-15" },
+        { name: "Barbell Curl / Cable Curls", sets: 3, reps: "10-12" }
     ],
     "Legs_gain": [
         { name: "Barbell Squat",       sets: 4, reps: "6-8" },
         { name: "Romanian Deadlift",   sets: 3, reps: "10-12" },
         { name: "Leg Press",           sets: 3, reps: "10-12" },
-        { name: "Walking Lunges",      sets: 3, reps: "12 each" },
+        { name: "Leg Extension",       sets: 3, reps: "12 each" },
         { name: "Leg Curl",            sets: 3, reps: "10-12" },
         { name: "Calf Raises",         sets: 4, reps: "12-15" }
+    ],
+    "Push_any" : [
+        { name: "Barbell Bench Press",    sets: 4, reps: "6-8" },
+        { name: "Overhead Press",         sets: 3, reps: "8-10" },
+        { name: "Incline Dumbbell Press", sets: 3, reps: "10-12" },
+        { name: "Lateral Raises",         sets: 3, reps: "10-12" },
+        { name: "Tricep Dips",            sets: 3, reps: "10-12" },
+        { name: "Tricep Extension / Skull Crushers", sets: 3, reps: "10-12" }
+    ],
+    "Pull_any" : [
+        { name: "Deadlift",               sets: 4, reps: "5-6" },
+        { name: "Pull-ups / Lat Pulldown",sets: 4, reps: "8-10" },
+        { name: "Barbell Row",            sets: 3, reps: "8-10" },
+        { name: "Cable Row",              sets: 3, reps: "10-12" },
+        { name: "Reverse Flys / Face Pull", sets: 3, reps: "12-15" },
+        { name: "Barbell Curl / Cable Curls", sets: 3, reps: "10-12" }
+    ], 
+    "Legs_any" : [
+        { name: "Barbell Squat",       sets: 4, reps: "6-8" },
+        { name: "Romanian Deadlift",   sets: 3, reps: "10-12" },
+        { name: "Leg Press",           sets: 3, reps: "10-12" },
+        { name: "Leg Extension",       sets: 3, reps: "12 each" },
+        { name: "Leg Curl",            sets: 3, reps: "10-12" },
+        { name: "Calf Raises",         sets: 4, reps: "12-15" }
+    ],
+    "Upper_any" : [
+        { name: "Bench Press",              sets: 3, reps: "8-12" },
+        { name: "Bent Over Row",            sets: 3, reps: "8-12" },
+        { name: "Overhead Press",           sets: 3, reps: "10-12" },
+        { name: "Lat Pulldown",             sets: 3, reps: "10-12" },
+        { name: "Dumbbell Flyes",           sets: 3, reps: "10-12" },
+        { name: "Bicep Curls / Tricep Pushdowns", sets: 3, reps: "10-12" }
+    ],
+    "Lower_any" : [
+        { name: "Squat",              sets: 3, reps: "8-12" },
+        { name: "Romanian Deadlift",  sets: 3, reps: "10-12" },
+        { name: "Leg Press",          sets: 3, reps: "10-12" },
+        { name: "Leg Extension",      sets: 3, reps: "12 each" },
+        { name: "Leg Curl",           sets: 3, reps: "10-12" },
+        { name: "Calf Raises",        sets: 3, reps: "10-12" }
+    ],
+    "Full Body_any" : [
+        { name: "Squat",              sets: 3, reps: "10-12" },
+        { name: "Bench Press",        sets: 3, reps: "10-12" },
+        { name: "Dumbbell Row",       sets: 3, reps: "10-12" },
+        { name: "Overhead Press",     sets: 3, reps: "10-12" },
+        { name: "Romanian Deadlift",  sets: 3, reps: "10-12" },
+        { name: "Plank",              sets: 3, reps: "30-60s" }
+    ],
+    "Chest_any" : [
+        { name: "Incline Bench Press / Incline Dumbbell Press",  sets: 3, reps: "8-10"},
+        { name: "Cable Flys",                                    sets: 3, reps: "8-10"},
+        { name: "Chest Dips",                                    sets: 3, reps: "8-10"},
+        { name: "Bench Press",                                   sets: 3, reps: "8-10"},
+    ],
+    "Back_any" : [
+        { name: "Pull-ups / Lat Pulldown",  sets: 3,    reps: "10-12"},
+        { name: "Barbell Row",              sets: 3,    reps: "8-10" },
+        { name: "Cable Row",                sets: 3,    reps: "10-12" },
+        { name: "Reverse Flys / Face Pull", sets: 3,    reps: "12-15" },
+        { name: "Single Arm Dumbbell Row",  sets: 3,    reps: "10-12"}
+    ],
+    "Shoulders_any" : [
+        { name: "Overhead Press",   sets: 3,    reps: "10-12"},
+        { name: "Lateral Raises",   sets: 3,    reps: "10-12"},
+        { name: "Reverse Flyes",    sets: 3,    reps: "10-12"},
+        { name: "Arnold Press",     sets: 3,    reps: "10-12"}
+    ],
+    "Biceps_any" : [
+        { name: "Cable Curl",      sets: 3, reps: "10-12"},
+        { name: "Barbell Curl",    sets: 3, reps: "10-12"},
+        { name: "Hammer Curls",    sets: 3, reps: "10-12"},
+        { name: "Incline Dumbbell Curl", sets: 3, reps: "10-12"}
+    ],
+    "Triceps_any" : [
+        { name: "Tricep Pushdowns", sets: 3, reps: "10-12"},
+        { name: "Tricep Extension", sets: 3, reps: "10-12"}, 
+        { name: "Tricep Dips",      sets: 3, reps: "10-12"},
+        { name: "Skull Crushers",   sets: 3, reps: "10-12"}
+    ],
+    "Arms_any" : [
+        { name: "Barbell Curl", sets: 3,    reps: "10-12"},
+        { name: "Hammer Curls", sets: 3,    reps: "10-12"},
+        { name: "Tricep Pushdowns", sets: 3,    reps: "10-12"},
+        { name: "Tricep Extension", sets: 3,    reps: "10-12"}
+    ],
+    "Glutes_any" : [
+        { name: "Hip Thrust",   sets: 3,    reps: "10-12"},
+        { name: "Glute Bridge", sets: 3,    reps: "10-12"},
+        { name: "Cable Kickback",   sets: 3,    reps: "10-12"},
+        { name: "Romanian Deadlift", sets: 3,   reps: "10-12"}
+    ],
+    "Hamstrings_any" : [
+        { name: "Romanian Deadlift",    sets: 3,    reps: "10-12"},
+        { name: "Leg Curl",             sets: 3,    reps: "10-12"},
+        { name: "Stiff-leg Deadlift",   sets: 3,    reps: "10-12"},
+        { name: "Walking Lunges",       sets: 3,    reps: "10-12"} 
+    ],
+    "Cardio_any" : [
+        { name: "Incline Treadmill Walking",    sets: 1,    reps: "20-30 minutes"},
+        { name: "Treadmill Running",            sets: 1,    reps: "20-30 minutes"},
+        { name: "Cycling",                      sets: 1,    reps: "20-30 minutes"},
+        { name: "Jump Rope",                    sets: 3,    reps: "5 minutes"},
+        { name: "Stair Master",                 sets: 1,    reps: "20-30 minutes"}
     ]
 }
 
@@ -88,11 +192,16 @@ function getRecommendedPlan(goal, activityLevel) {
 }
 
 function getExercisesForDay(dayType, goal, fallbackGoal = null){
-    const key = `${dayType}_${goal.replace("-weight", "")}`
-    const result = excercise_db[key]
-    if (result && result.length) return result
-    if (fallbackGoal) return excercise_db[`${dayType}_${fallbackGoal}`] || []
-    return []
+    const goalSuffix = goal.replace("-weight", "");
+    const primaryKey = `${dayType}_${goalSuffix}`;
+    const fallbackKey = `${dayType}_${fallbackGoal}`;
+    const anyKey = `${dayType}_any`;
+    //1. try specific goal
+    if (excercise_db[primaryKey]) return excercise_db[primaryKey];
+    //2. try fallback goal (adding/replacing word)
+    if (fallbackGoal && excercise_db[fallbackKey]) return excercise_db[fallbackKey]; 
+    //3. try ANY of the exercise available in the db
+    return excercise_db[anyKey] || [];
 }
 
 function renderExerciseDetails(schedule, goal, splitType = null){
@@ -159,7 +268,3 @@ function renderPersonalizedCard(plan){
 }
 
 export {getRecommendedPlan, renderPersonalizedCard, renderExerciseDetails}
-
-
-
-
